efcaa77919e03530f5a369158214af92881103ad
ston3o <5039608+ston3o@users.noreply.github.com>
2015-08-26 07:05:43 +0200

    Update i3 config

--- .i3/i3status.sh
+++ .i3/i3status.sh
@@ -3,7 +3,7 @@
 i3status --config ~/.i3/i3status.conf | (read line && echo $line && read line && echo $line && while :
 do
   read line
-  play=$(ncmpcpp --now-playing "{{%a - }%t}|{%f}")
+  play=$(mpc current)
   play="[{ \"full_text\": \"${play}\" },"
   echo "${line/[/$play}" || exit 1
 done)
