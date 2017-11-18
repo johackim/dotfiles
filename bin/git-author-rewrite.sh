2f600619d3ad0b5bc79f136cf6398c332fd20181
johackim <contact@johackim.com>
2017-11-18 02:37:14 +0100

    Update scripts

---
+++ bin/git-author-rewrite.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+
+OLD_NAME="OLD_NAME"
+CORRECT_NAME="CORRECT_NAME"
+CORRECT_EMAIL="CORRECT_EMAIL"
+
+if [ "$GIT_COMMITTER_NAME" = "$OLD_NAME" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_NAME" = "$OLD_NAME" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
