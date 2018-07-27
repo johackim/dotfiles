eec365037b033935a7614ec820e9d38a29eeebb0
johackim <contact@johackim.com>
2018-07-27 10:39:28 +0200

    Add i3blocks

--- .i3/i3status.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-i3status --config ~/.i3/i3status.conf | (read line && echo $line && read line && echo $line && while :
-do
-  read line
-  play=$(mpc current)
-  play="[{ \"full_text\": \"${play}\" },"
-  echo "${line/[/$play}" || exit 1
-done)
