0ccfba11614765a69e402a3a8ed76107694f06f6
johackim <contact@johackim.com>
2022-11-29 10:14:32 +0100

    fix: update script

--- .local/bin/shortform.sh
+++ .local/bin/shortform.sh
@@ -7,6 +7,6 @@ OLD_SUMMARY=$(cat $CACHE_FILE 2>/dev/null)
 NEW_SUMMARY=$(curl -s -H "Authorization: Basic $SHORTFORM_API_KEY" -H "X-Sf-Client: 11.7.0" "https://www.shortform.com/api/books/?sort=date" | jq -r '.data | sort_by(.created) | last | .title')
 
 if [[ "$OLD_SUMMARY" != "$NEW_SUMMARY" && -n "$NEW_SUMMARY" ]]; then
-    ~/.local/.bin/alert.sh "Shortform" "Nouveau résumés"
-    echo "'$NEW_SUMMARY'" > $CACHE_FILE
+    ~/.local/bin/alert.sh "Shortform" "Nouveau résumés"
+    echo "$NEW_SUMMARY" > $CACHE_FILE
 fi
