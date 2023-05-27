1ecf73a4d6f68d9484d7136665c96a95923af6d6
johackim <contact@johackim.com>
2023-05-28 00:35:44 +0200

    feat: update subtitles.sh script

--- .local/bin/subtitles.sh
+++ .local/bin/subtitles.sh
@@ -4,14 +4,17 @@ if [ -f "$1.srt" ]; then
     exit 1
 fi
 
+FILENAME=$(basename "$1")
+
 if [[ $1 =~ ^https?:// ]]; then
-    yt-dlp $1 --extract-audio --audio-format mp3 -o file.mp3
-    yt-dlp $1 -o 'file.%(ext)s'
+    FILENAME=$(yt-dlp --get-title $1)
+    yt-dlp $1 -f mp4 -o "${FILENAME}.%(ext)s" > /dev/null 2>&1
+    yt-dlp $1 -f mp4 --extract-audio --audio-format mp3 -k -o "${FILENAME}.mp3" > /dev/null 2>&1
 else
-    ffmpeg -i "$1" -fs 25M -vn -acodec libmp3lame -ac 2 -qscale:a 4 -ar 48000 "$1.mp3" > /dev/null 2>&1
+    ffmpeg -i "$1" -fs 25M -vn -acodec libmp3lame -ac 2 -qscale:a 4 -ar 48000 "${FILENAME}.mp3" > /dev/null 2>&1
 fi
 
-openai api audio.transcribe -f "file.mp3" --response-format srt > file.srt 2>&1
-sed -i -e :a -e '/^\n*$/{$d;N;ba' -e '}' file.srt
-sed -i '/Upload progress/d' file.srt
-trash file.mp3
+openai api audio.transcribe -f "${FILENAME}.mp3" --response-format srt > "${FILENAME}.srt" 2>&1
+sed -i -e :a -e '/^\n*$/{$d;N;ba' -e '}' "${FILENAME}.srt"
+sed -i '/Upload progress/d' "${FILENAME}.srt"
+trash "${FILENAME}.mp3"
