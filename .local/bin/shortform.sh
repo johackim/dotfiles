17ea24ec427cc6cd26ab842bdb60d69cc092ec84
johackim <contact@johackim.com>
2023-12-22 18:51:57 +0100

    feat: update shortform script

--- .local/bin/shortform.sh
+++ .local/bin/shortform.sh
@@ -2,7 +2,7 @@
 
 [ -f ~/.private_aliases ] && source ~/.private_aliases
 
-CACHE_FILE="/tmp/.shortform"
+CACHE_FILE="$HOME/.shortform"
 OLD_SUMMARY=$(cat $CACHE_FILE 2>/dev/null)
 NEW_SUMMARY=$(curl -s -H "Authorization: Basic $SHORTFORM_API_KEY" -H "X-Sf-Client: 11.7.0" "https://www.shortform.com/api/books/?sort=date" | jq -r '.data | sort_by(.created) | last | .title')
 
