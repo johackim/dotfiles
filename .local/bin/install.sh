f4cd3a4f615a5d58d930e2f2eb913df5fc922582
johackim <contact@johackim.com>
2023-12-31 02:24:33 +0100

    feat: update install.sh script

--- .local/bin/install.sh
+++ .local/bin/install.sh
@@ -60,7 +60,7 @@ sudo systemctl enable --now bluetooth > /dev/null 2>&1
 
 echo "Install fonts..."
 
-sudo pacman -S nerd-fonts ttf-inconsolata-nerd noto-fonts-emoji gnu-free-fonts > /dev/null 2>&1
+sudo pacman -S --noconfirm --needed nerd-fonts ttf-inconsolata-nerd noto-fonts-emoji gnu-free-fonts > /dev/null 2>&1
 yay -S --sudoloop --noconfirm --needed fonts-noto-hinted ttf-ms-fonts > /dev/null 2>&1
 
 echo "Install nodejs..."
@@ -83,7 +83,7 @@ echo "Install wpg..."
 
 sudo pacman -S --noconfirm --needed python python-pip python-gobject python-pillow > /dev/null 2>&1
 sudo pacman -S --noconfirm --needed wget feh > /dev/null 2>&1
-sudo pip install pywal > /dev/null 2>&1
+sudo pip install pywal --break-system-packages > /dev/null 2>&1
 yay -S --sudoloop --noconfirm --needed --overwrite="*" themix-full-git wpgtk > /dev/null 2>&1
 
 echo "Install tmux..."
@@ -97,23 +97,21 @@ sudo pacman -S --noconfirm --needed chromium > /dev/null 2>&1
 
 echo "Install neovim..."
 
-sudo pacman -S --noconfirm --needed fuse > /dev/null 2>&1
-sudo wget -O /usr/local/bin/nvim https://github.com/neovim/neovim/releases/download/v0.7.2/nvim.appimage > /dev/null 2>&1
-sudo chmod +x /usr/local/bin/nvim > /dev/null 2>&1
+sudo pacman -S --noconfirm --needed neovim > /dev/null 2>&1
 curl -sfLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim > /dev/null 2>&1
 nvim +PlugInstall +qa! > /dev/null 2>&1
 
 echo "Install spotify..."
 
 yay -S --sudoloop --noconfirm --needed spotify ffmpeg-compat-57 > /dev/null 2>&1
-sudo pip install -U spotify-cli-linux dbus-python > /dev/null 2>&1
+sudo pip install -U spotify-cli-linux dbus-python --break-system-packages > /dev/null 2>&1
 
 echo "Install misc tools..."
 
-sudo pacman -S --noconfirm --needed --overwrite="*" restic thunar scrot eog mplayer trash-cli inetutils gnome-keyring pkgfile fd net-tools keynav mousepad cronie signal-desktop qbittorrent firefox docker evince calibre the_silver_searcher telegram-desktop mpv aria2 ncdu brightnessctl > /dev/null 2>&1
-yay -S --sudoloop --noconfirm --needed masterpassword-cli portmaster-stub-bin polkit-dumb-agent-git > /dev/null 2>&1
-sudo systemctl enable --now cronie portmaster docker > /dev/null 2>&1
-sudo -E pip install yt-dlp httpie > /dev/null 2>&1
+sudo pacman -S --noconfirm --needed --overwrite="*" restic thunar scrot eog mplayer trash-cli inetutils gnome-keyring pkgfile fd net-tools mousepad cronie signal-desktop qbittorrent firefox docker evince calibre the_silver_searcher telegram-desktop mpv aria2 ncdu brightnessctl cronie polkit-kde-agent arandr i3status > /dev/null 2>&1
+yay -S --sudoloop --noconfirm --needed masterpassword-cli keynav > /dev/null 2>&1
+sudo systemctl enable --now cronie docker > /dev/null 2>&1
+sudo pip install yt-dlp httpie --break-system-packages > /dev/null 2>&1
 
 # Reload session manually
 # Apply Gtk and icons theme with lxappearance manually
