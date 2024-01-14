957a9bc4a889677812a3fedc87e964fb3a1b2a8b
johackim <contact@johackim.com>
2024-01-14 21:39:46 +0100

    refactor: update shortform.sh script

--- .local/bin/shortform.sh
+++ .local/bin/shortform.sh
@@ -3,10 +3,18 @@
 [ -f ~/.private_aliases ] && source ~/.private_aliases
 
 CACHE_FILE="$HOME/.shortform"
+API_URL="https://www.shortform.com/api/books/?sort=date"
+API_HEADERS=("Authorization: Basic $SHORTFORM_API_KEY" "X-Sf-Client: 11.7.0")
+
+get_latest_summary() {
+    curl -s -H "${API_HEADERS[0]}" -H "${API_HEADERS[1]}" "$API_URL" | \
+    jq -r '.data | sort_by(.created) | last | .title'
+}
+
 OLD_SUMMARY=$(cat $CACHE_FILE 2>/dev/null)
-NEW_SUMMARY=$(curl -s -H "Authorization: Basic $SHORTFORM_API_KEY" -H "X-Sf-Client: 11.7.0" "https://www.shortform.com/api/books/?sort=date" | jq -r '.data | sort_by(.created) | last | .title')
+NEW_SUMMARY=$(get_latest_summary)
 
 if [[ "$OLD_SUMMARY" != "$NEW_SUMMARY" && -n "$NEW_SUMMARY" ]]; then
-    ~/.local/bin/alert.sh "Shortform" "Nouveau résumés"
-    echo "$NEW_SUMMARY" > $CACHE_FILE
+    $HOME/.local/bin/alert.sh "Shortform" "Nouveau résumés"
+    echo "$NEW_SUMMARY" > "$CACHE_FILE"
 fi
