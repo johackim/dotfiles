8f66e82464cd53c336fca0e023b7c6b3b7b1bd15
johackim <contact@johackim.com>
2024-04-15 12:52:25 +0200

    feat: update organize.sh script

--- .local/bin/organize.sh
+++ .local/bin/organize.sh
@@ -4,7 +4,7 @@ shopt -s nocaseglob
 cd ~/Downloads || exit
 
 mkdir -p images
-fd -j1 -d1 -epng -esvg -ejpg -ejpeg -egif -epsd -x mv -n {} images
+fd -j1 -d1 -epng -esvg -ejpg -ejpeg -egif -epsd -ewebp -x mv -n {} images
 
 mkdir -p textes
 fd -j1 -d1 -esql -ertf -eodt -ejson -eyml -eyaml -exml -exls -etxt -eopml -epo -epdf -eepub -emobi -ecrt -edoc -edocx -exlsx -ehtml -ecsv -emd -etorrent -earia2 -ekey -esh -epy -eovpn -elog -econf -eods -exmind -egpx -edb -edrawio -eazw -eazw3 -eencrypted -eAppImage -em3u-etextes -x mv -n {} textes
@@ -13,7 +13,7 @@ mkdir -p videos
 fd -j1 -d1 -eavi -emkv -em4v -ewebm -emp4 -esrt -eogv -x mv -n {} videos
 
 mkdir -p archives
-fd -j1 -d1 -ezip -etar.gz -etgz -egz -ebundle -eapk -erar -e7z -eiso -eexe -ebz2 -etar -eimg -exz -x mv -n {} archives
+fd -j1 -d1 -ezip -etar.gz -etgz -egz -ebundle -eapk -erar -e7z -eiso -eexe -ebz2 -etar -eimg -exz -eab -x mv -n {} archives
 
 mkdir -p music
 fd -j1 -d1 -ewav -emp3 -x mv {} music
