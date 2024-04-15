9903598c8a0d6dc0d2295ac04ebabc5a408c3fce
johackim <contact@johackim.com>
2024-04-15 18:02:54 +0200

    feat: update ccleaner.sh

--- .local/bin/ccleaner.sh
+++ .local/bin/ccleaner.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 sudo pacman --noconfirm -R $(pacman -Qtdq) > /dev/null 2>&1
-sudo pacman -Scc --noconfirm > /dev/null 2>&1
+yes | sudo pacman -Scc > /dev/null 2>&1
 yay -Yc --noconfirm > /dev/null 2>&1
-sudo docker system prune -af > /dev/null 2>&1
-sudo rm -rf ~/.cache
-trash-empty -f
+sudo docker system prune -af --volumes > /dev/null 2>&1
+journalctl --vacuum-size=500M > /dev/null 2>&1
+trash-empty -f > /dev/null 2>&1
