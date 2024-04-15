2704e6f788d48ce8a3c6d4d0424999b9c536a168
johackim <contact@johackim.com>
2024-04-15 12:49:01 +0200

    feat: update keyboard and touchpad settings

--- .local/bin/init.sh
+++ .local/bin/init.sh
@@ -6,6 +6,8 @@ dbus-update-activation-environment --systemd DISPLAY
 eval "$(/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh)"
 export SSH_AUTH_SOCK
 xrandr -s 1920x1080
+setxkbmap us_intl,us,fr grp:rctrl_rshift_toggle
+synclient TapButton1=1 TapButton2=3
 
 wpg -a ~/.dotfiles/wallpaper.jpg > /dev/null 2>&1
 wpg -s wallpaper.jpg > /dev/null 2>&1
