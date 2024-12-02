c588c1d19350494b440a3c9132b006f8224ed99b
johackim <contact@johackim.com>
2024-12-02 14:38:54 +0100

    feat: update init.sh

--- .local/bin/init.sh
+++ .local/bin/init.sh
@@ -5,7 +5,6 @@
 dbus-update-activation-environment --systemd DISPLAY
 eval "$(/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh)"
 export SSH_AUTH_SOCK
-xrandr -s 1920x1080
 setxkbmap us_intl,us,fr grp:rctrl_rshift_toggle
 synclient TapButton1=1 TapButton2=3
 
