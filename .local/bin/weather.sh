cc431e0fe7b20a57fdcbc1ae14cac8db40bbcc1a
johackim <contact@johackim.com>
2025-02-19 23:53:52 +0100

    feat: update weather script

--- .local/bin/weather.sh
+++ .local/bin/weather.sh
@@ -5,7 +5,7 @@ CITY_ID="6454307"
 
 WEATHER_URL="http://api.openweathermap.org/data/2.5/weather?id=${CITY_ID}&appid=${API_KEY}&units=metric"
 WEATHER_INFO=$(wget -qO- "${WEATHER_URL}")
-WEATHER_TEMP=$(echo "${WEATHER_INFO}" | jq '.main.temp' | awk '{print int($1+0.5)}')
+WEATHER_TEMP=$(echo "${WEATHER_INFO}" | jq '.main.temp' 2>/dev/null | awk '{print int($1+0.5)}')
 
 [[ ! $WEATHER_TEMP ]] && exit 1
 
