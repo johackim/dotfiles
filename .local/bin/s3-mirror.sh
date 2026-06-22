165f52738e7d6aaa307589599c8c2da08ebfc675
johackim <contact@johackim.com>
2026-06-22 19:22:28 +0200

    feat: update s3-mirror.sh

--- .local/bin/s3-mirror.sh
+++ .local/bin/s3-mirror.sh
@@ -6,18 +6,11 @@ SRC=$1
 DEST=$2
 WEBHOOK_URL=$3
 
-DATE=$(date +%Y-%m-%d)
 BUCKETS=$(timeout 30s rclone lsf --dir-slash=false "$SRC":)
 
 for b in $BUCKETS; do
     echo "Syncing bucket: $b";
-
-    rclone sync --fast-list --checksum --transfers 32 --checkers 32 \
-        --exclude ".archives/**" \
-        --backup-dir "$DEST:$b/.archives/$DATE" \
-        "$SRC:$b" "$DEST:$b";
-
-    rclone delete "$DEST:$b/.archives/" --min-age 30d --rmdirs;
+    rclone sync --fast-list --transfers 32 --checkers 32 "$SRC:$b" "$DEST:$b";
 done
 
 curl -s "$WEBHOOK_URL"
