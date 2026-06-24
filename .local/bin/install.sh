2f94ee61b4a6f2a9e2dbf33bb9680a276f1d5207
johackim <contact@johackim.com>
2026-06-24 09:42:53 +0200

    feat: update install.sh

--- .local/bin/install.sh
+++ .local/bin/install.sh
@@ -69,7 +69,7 @@ sudo pacman -S --noconfirm --needed dunst
 
 echo "Install pipewire..."
 
-sudo pacman -S --noconfirm --needed pipewire pipewire-alsa pipewire-pulse pipewire-jack pipewire-audio pavucontrol wireplumber
+sudo pacman -S --noconfirm --needed pipewire pipewire-alsa pipewire-pulse pipewire-jack pipewire-audio pavucontrol wireplumber sof-firmware
 
 echo "Install touchpad..."
 
@@ -123,7 +123,7 @@ nvim +PlugInstall +sleep 3 +qa!
 
 echo "Install misc tools..."
 
-sudo pacman -S --noconfirm --needed --overwrite="*" restic thunar thunar-volman gvfs-mtp scrot eog mplayer trash-cli inetutils gnome-keyring pkgfile fd net-tools mousepad cronie signal-desktop qbittorrent firefox docker evince ripgrep-all telegram-desktop mpv aria2 ncdu brightnessctl cronie polkit-kde-agent arandr i3status gnome-calculator gcolor3 flameshot file-roller 7zip
+sudo pacman -S --noconfirm --needed --overwrite="*" restic thunar thunar-volman gvfs-mtp scrot eog mplayer trash-cli inetutils gnome-keyring pkgfile fd net-tools mousepad cronie signal-desktop qbittorrent firefox docker evince ripgrep-all telegram-desktop mpv aria2 ncdu brightnessctl cronie polkit-kde-agent arandr i3status gnome-calculator gcolor3 flameshot file-roller 7zip playerctl
 yay -S --sudoloop --noconfirm --needed masterpassword-cli keynav calibre-installer
 sudo systemctl enable --now cronie docker
 uv tool install yt-dlp
